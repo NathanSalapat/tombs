47d4c4de354ad16cf69b545d65d89bc9ea2d15d7
NathanSalapat <rendertracer@gmail.com>
2018-12-19 08:22:43 -0600

    added settings to disable mod support.

--- init.lua
+++ init.lua
@@ -8,10 +8,10 @@ dofile(minetest.get_modpath('tombs')..'/gravestones.lua')
 dofile(minetest.get_modpath('tombs')..'/formspec.lua')
 
 --support for other mods
-if minetest.get_modpath('default') then
+if minetest.get_modpath('default') and (minetest.settings:get_bool('tombs.load_default')) then
    dofile(minetest.get_modpath('tombs')..'/default.lua')
 end
 
-if minetest.get_modpath('wool') then
+if minetest.get_modpath('wool') and (minetest.settings:get_bool('tombs.load_wool')) then
    dofile(minetest.get_modpath('tombs')..'/wool.lua')
 end
